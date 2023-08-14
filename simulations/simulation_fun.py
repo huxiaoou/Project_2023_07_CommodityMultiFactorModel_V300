2fccb9365d982c7db1526447ddb5d72da17728a9
huxiaoou <516984451@qq.com>
2023-08-14 15:53:17 +0800

    Add raw simulation

---
+++ simulations/simulation_fun.py
@@ -0,0 +1,87 @@
+import os
+import datetime as dt
+import itertools as ittl
+import multiprocessing as mp
+from skyrim.whiterun import CCalendar, CInstrumentInfoTable
+from skyrim.riverwood2 import CManagerMarketData, CManagerSignalOpt, CPortfolio
+from skyrim.winterhold import remove_files_in_the_dir, check_and_mkdir
+from skyrim.falkreath import CLib1Tab1
+
+
+def simulation_single_factor(
+        signal_id: str, hold_period_n: int, start_delay: int,
+        calendar_path: str, instrument_info_path: str,
+        md_by_instru_dir: str, major_minor_dir: str, available_universe_dir: str,
+        sig_dir: str, dst_dir: str,
+        database_structure: dict[str, CLib1Tab1],
+        test_universe: list[str], test_bgn_date: str, test_stp_date: str,
+        cost_reservation: float, cost_rate: float, init_premium: float,
+        skip_if_exist: bool
+):
+    # --- pid
+    port_id = "{}.HPN{:03d}.D{:02d}".format(signal_id, hold_period_n, start_delay)
+    nav_file = "{}.nav.daily.csv.gz".format(port_id)
+    nav_path = os.path.join(dst_dir, port_id, nav_file)
+    if skip_if_exist and os.path.exists(nav_path):
+        return 0
+
+    # --- tips
+    t0 = dt.datetime.now()
+    print("| {} | {:>60s} | calculating ... |".format(t0, port_id))
+
+    # --- directory check
+    dir_pid = os.path.join(dst_dir, port_id)
+    dir_pid_trades = os.path.join(dir_pid, "trades")
+    dir_pid_positions = os.path.join(dir_pid, "positions")
+    check_and_mkdir(dir_pid)
+    check_and_mkdir(dir_pid_trades)
+    check_and_mkdir(dir_pid_positions)
+    remove_files_in_the_dir(dir_pid_trades)
+    remove_files_in_the_dir(dir_pid_positions)
+
+    # 3 - load calendar and hist dates list
+    trade_calendar = CCalendar(calendar_path)
+
+    # 4 - load aux data
+    instrument_info = CInstrumentInfoTable(t_path=instrument_info_path, t_index_label="windCode", t_type="CSV")
+    manager_md = CManagerMarketData(t_mother_universe=test_universe, t_dir_market_data=md_by_instru_dir, t_dir_major_data=major_minor_dir)
+    manager_signal = CManagerSignalOpt(
+        t_mother_universe=test_universe, t_available_universe_dir=available_universe_dir,
+        t_database_structure=database_structure,
+        t_factors_by_tm_dir=sig_dir, t_factor_lbl=signal_id,
+        t_mgr_md=manager_md,
+        t_is_trend_follow=True, t_print_details=False
+    )
+
+    # 5 - simulation main loop
+    simu_portfolio = CPortfolio(
+        t_pid=port_id,
+        t_init_cash=init_premium, t_cost_reservation=cost_reservation, t_cost_rate=cost_rate,
+        t_dir_pid=dir_pid, t_dir_pid_trades=dir_pid_trades, t_dir_pid_positions=dir_pid_positions,
+        t_save_details=False
+    )
+    simu_portfolio.main_loop(
+        t_simu_bgn_date=test_bgn_date, t_simu_stp_date=test_stp_date, t_start_delay=start_delay, t_hold_period_n=hold_period_n,
+        t_trade_calendar=trade_calendar, t_instru_info=instrument_info, t_mgr_signal=manager_signal, t_mgr_md=manager_md
+    )
+
+    # --- end tips
+    t1 = dt.datetime.now()
+    print("| {} | {:>60s} | calculated  ... | time consuming = {:>6.2f} seconds |".format(t1, port_id, (t1 - t0).total_seconds()))
+    return 0
+
+
+def cal_simulations_mp(
+        proc_num: int,
+        signal_ids: list[str], test_windows: list[int],
+        **kwargs):
+    t0 = dt.datetime.now()
+    pool = mp.Pool(processes=proc_num)
+    for signal_id, test_window in ittl.product(signal_ids, test_windows):
+        for start_delay in range(test_window):
+            pool.apply_async(simulation_single_factor, args=(signal_id, test_window, start_delay), kwds=kwargs)
+    pool.close()
+    pool.join()
+    t1 = dt.datetime.now()
+    print("... total time consuming: {:.2f} seconds".format((t1 - t0).total_seconds()))
+    return 0
--- test.py
+++ test.py
@@ -1,18 +1,52 @@
-from signals.signals_cls import CSignalHedge, CCalendar
-from setup_project import calendar_path, available_universe_dir, signals_hedge_test_dir, factors_exposure_dir, factors_exposure_neutral_dir
+from setup_project import (calendar_path, instrument_info_path, md_by_instru_dir, available_universe_dir,
+                           signals_hedge_test_dir, simulations_hedge_test_dir, factors_exposure_dir, factors_exposure_neutral_dir)
 
-run_mode, bgn_date, stp_date = "O", "20150701", "20230801"
+test_id = ["sig", "simu"][1]
 
-calendar = CCalendar(calendar_path)
-shared_args = dict(available_universe_dir=available_universe_dir, sig_save_dir=signals_hedge_test_dir, calendar=calendar)
-s = CSignalHedge(uni_prop=0.2,
-                 src_factor_dir=factors_exposure_dir, src_factor_id="BASISA126",
-                 **shared_args)
+if test_id == "sig":
+    from signals.signals_cls import CSignalHedge, CCalendar
 
-s.main(run_mode, bgn_date, stp_date)
+    run_mode, bgn_date, stp_date = "O", "20150701", "20230801"
+    calendar = CCalendar(calendar_path)
+    shared_args = dict(available_universe_dir=available_universe_dir, sig_save_dir=signals_hedge_test_dir, calendar=calendar)
+    s = CSignalHedge(uni_prop=0.2,
+                     src_factor_dir=factors_exposure_dir, src_factor_id="BASISA126",
+                     **shared_args)
 
-s = CSignalHedge(uni_prop=0.2,
-                 src_factor_dir=factors_exposure_neutral_dir, src_factor_id="BASISA126_WS",
-                 **shared_args)
-s.main(run_mode, bgn_date, stp_date)
-print(s.sig_id)
+    s.main(run_mode, bgn_date, stp_date)
+
+    s = CSignalHedge(uni_prop=0.2,
+                     src_factor_dir=factors_exposure_neutral_dir, src_factor_id="BASISA126_WS",
+                     **shared_args)
+    s.main(run_mode, bgn_date, stp_date)
+elif test_id == "simu":
+    import os
+    from setup_project import futures_by_instrument_dir
+    from config_project import concerned_instruments_universe
+    from simulations.simulation_fun import simulation_single_factor
+    from struct_lib.returns_and_exposure import get_lib_struct_available_universe
+    from struct_lib.portfolios import get_signal_lib_struct
+
+    major_minor_dir = os.path.join(futures_by_instrument_dir, "major_minor")
+
+    bgn_date, stp_date = "20150701", "20230801"
+    # signal_id = "BASISA126_UHP04"
+    # signal_id = "LIQUIDBD010_WS_UHP02"
+    # signal_id = "LIQUIDBD010_WS_UHP04"
+    # signal_id = "MTM_WS_UHP02"
+    signal_id = "MTM_WS_UHP04"
+    database_structure = {
+        "available_universe": get_lib_struct_available_universe(),
+        signal_id: get_signal_lib_struct(signal_id)
+    }
+
+    simulation_single_factor(
+        signal_id=signal_id, hold_period_n=1, start_delay=0,
+        calendar_path=calendar_path, instrument_info_path=instrument_info_path,
+        md_by_instru_dir=md_by_instru_dir, major_minor_dir=major_minor_dir, available_universe_dir=available_universe_dir,
+        sig_dir=signals_hedge_test_dir, dst_dir=simulations_hedge_test_dir,
+        database_structure=database_structure, test_universe=concerned_instruments_universe,
+        test_bgn_date=bgn_date, test_stp_date=stp_date,
+        cost_reservation=0, cost_rate=3e-4, init_premium=1e8,
+        skip_if_exist=False,
+    )
